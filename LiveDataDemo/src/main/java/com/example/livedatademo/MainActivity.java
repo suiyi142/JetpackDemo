9533489c165459b4364a86f1255e370235bcc794
suiyi <Yi.Sui@desay-svautomotive.com>
2021-03-13 16:02:37 +0800

    lifeData：完成部分代码

--- LiveDataDemo/src/main/java/com/example/livedatademo/MainActivity.java
+++ LiveDataDemo/src/main/java/com/example/livedatademo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.livedatademo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+
+        PeopleLiveData peopleLiveData = new PeopleLiveData(new PeopleBean("张三", 2));
+//        tv.setText("name = " + peopleLiveData.getValue().getName() + " age = " + peopleLiveData.getValue().getAge());
+
+        peopleLiveData.observe(this, new Observer<PeopleBean>() {
+            @Override
+            public void onChanged(PeopleBean peopleBean) {
+                Log.d(TAG, "onChanged() called with: peopleBean = [" + peopleBean + "]");
+                tv.setText("name = " + peopleBean.getName() + " age = " + peopleBean.getAge());
+            }
+        });
     }
 }
--- LiveDataDemo/src/main/java/com/example/livedatademo/PeopleLiveData.java
+++ LiveDataDemo/src/main/java/com/example/livedatademo/PeopleLiveData.java
@@ -1,17 +1,21 @@
 package com.example.livedatademo;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 
 /**
  * @CreateDate: 2021-3-10 8:50
  * @UpdateDate: 2021-3-10 8:50
- * @Description:
+ * @Description: activity在前台时2s人的age+1，在后台时停止长大
  * @Author: uidq2189
  * @UpdateRemark:
  * @Version: 1.0
  */
 public class PeopleLiveData extends LiveData<PeopleBean> {
+    private static final String TAG = "PeopleLiveData";
     private PeopleBean peopleBean;
+    private boolean isRun = false;
 
     public PeopleLiveData(PeopleBean peopleBean) {
         this.peopleBean = peopleBean;
@@ -19,12 +23,30 @@ public class PeopleLiveData extends LiveData<PeopleBean> {
 
     @Override
     protected void onActive() {
+        Log.d(TAG, "onActive() called");
         super.onActive();
+        isRun = true;
+        new Thread(runnable).start();
     }
 
     @Override
     protected void onInactive() {
+        Log.d(TAG, "onInactive() called");
         super.onInactive();
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            while (isRun){
+                peopleBean.setAge(peopleBean.getAge()+1);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
 
 }
